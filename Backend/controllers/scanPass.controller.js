657e2c5f08dcafb75a06b3ad354ec79bd6859a9b
soham323 <118099505+soham323@users.noreply.github.com>
2024-09-22 16:33:18 +0530

    scan pass module completed and tested on postman

--- Backend/controllers/scanPass.controller.js
+++ Backend/controllers/scanPass.controller.js
@@ -1,35 +1,42 @@
 import { Pass } from "../models/pass.model.js"; 
 
-const scanPass = async (req, res) => {
-    try {
-      const { passId, eventName } = req.body;
-  
-      // Find the pass based on both qrData and eventName
-      const pass = await Pass.findOne({ passId, eventName });
-  
-      // If no pass found, return an error
-      if (!pass) {
-        return res.status(404).json({ 
-          message: 'Pass not found for this event',
-         });
-      }
-  
-      // If pass is already scanned, return an error
-      if (pass.scanned) {
-        return res.status(400).json({ message: 'Pass already scanned' });
-      }
-  
-      // Mark the pass as scanned
-      pass.scanned = true;
-      await pass.save();
-  
-      // Respond with success
-      res.status(200).json({ message: 'Pass scanned successfully', pass });
-    } catch (error) {
-      console.error('Error while scanning the pass:', error);
-      res.status(500).json({ message: 'Server error' });
-    }
-  };
+// Controller to scan pass with mobile qr
 
-  export default scanPass;
-  
+export const scanPass = async (req, res) => {
+  const { passId }= req.body;
+
+  try {
+    const pass = await Pass.findOne({
+      passId 
+    });
+
+    if (!pass) {
+      return res.status(404).json({
+        success: false,
+        message: "Pass not found for this event",
+      });
+    }
+    if (pass.scanned) {
+      return res.status(400).json({
+        success: false,
+        message: "Pass already scanned!",
+      })
+    }
+
+    // Scan pass if all above switch statements get false
+    pass.scanned = true;
+    await pass.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Pass Scanned Successfully!",
+    })
+  }
+  catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error while Scanning pass",
+      error: error.message,
+    });
+  }
+};
--- Backend/routes/eventRoute.js
+++ Backend/routes/eventRoute.js
@@ -1,7 +1,16 @@
 import express from "express";
 import { createEvent } from "../controllers/createEvent.controller.js";
 import { getEventPasses } from "../controllers/fetchPasses.controller.js";
+import { scanPass } from "../controllers/scanPass.controller.js";   
 const router = express.Router();
+//route to create event and their passes
 router.post('/create-event', createEvent);
+
+// route to get all passes data
 router.get('/event/:eventId/passes', getEventPasses);
+
+// route to scan a pass
+router.post("/scan-pass", scanPass);
+
 export default router;
+
